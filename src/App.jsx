7d898a0e2cda55008a9c169c6b1c5715cc79535d
Doddanna <doddannab17@gmail.com>
2024-11-11 23:12:11 +0530

    Counter example with UseState with import

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css';
-import ErrorExample from './tutorial/01-useState/starter/01-error-example';
+import "./App.css";
+// import ErrorExample from "./tutorial/01-useState/starter/01-error-example";
 
+import UseStateBasics from "./tutorial/01-useState/starter/02-useState-basics";
 function App() {
   return (
     <>
-      <ErrorExample />
+      {/* <ErrorExample /> */}
       <p>Advanced React App</p>
+      <UseStateBasics />
     </>
   );
 }
--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h2>useState basics</h2>
+      <div className="count">{count}</div>
+      <button type="button" className="btn" onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </>
+  );
 };
 
 export default UseStateBasics;
