adf8ed11528a9bba852ed29b71e49f2c4c8323f8
Doddanna <doddannab17@gmail.com>
2024-11-11 23:34:08 +0530

    List example with useState to remove single or all items

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 // import ErrorExample from "./tutorial/01-useState/starter/01-error-example";
-
-import UseStateBasics from "./tutorial/01-useState/starter/02-useState-basics";
+// import UseStateBasics from "./tutorial/01-useState/starter/02-useState-basics";
+import UseStateArray from "./tutorial/01-useState/starter/03-useState-array";
 function App() {
   return (
     <>
       {/* <ErrorExample /> */}
       <p>Advanced React App</p>
-      <UseStateBasics />
+      {/* <UseStateBasics /> */}
+      <UseStateArray />
     </>
   );
 }
--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,47 @@
+import { useState } from "react";
+import { data } from "../../../data";
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [persons, setPersons] = useState(data);
+
+  function removePerson(id) {
+    const newData = persons
+      .filter((person) => person.id != id)
+      .map((person) => person);
+    setPersons(newData);
+  }
+
+  function removeAllPersons() {
+    setPersons([]);
+  }
+
+  return (
+    <>
+      <ul className="user-container1">
+        {persons.map((line) => {
+          return (
+            <div key={line.id}>
+              <div className="item">{line.name}</div>
+              <button
+                onClick={() => {
+                  removePerson(line.id);
+                }}
+              >
+                Remove from list
+              </button>
+            </div>
+          );
+        })}
+      </ul>
+      <button
+        type="button"
+        onClick={() => {
+          removeAllPersons();
+        }}
+      >
+        Clear All
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
